3d698f0ec4ae521904201e0154a41e732c8852a8
Mysciak <109069197+mysci4k@users.noreply.github.com>
2025-12-20 22:17:28 +0100

    Revert "[RELEASE] v0.1.0 (#14)"
    This reverts commit c8211b3ec983f6912fd87b800770309d3cbcc188.

--- src/domain/events/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod board_event;
-pub mod event_bus;
-
-pub use board_event::*;
-pub use event_bus::{EventBus, SharedEventBus};
--- src/domain/services/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod email_service;
-pub mod token_service;
-
-pub use email_service::{EmailService, EmailTemplate};
-pub use token_service::TokenService;
